20f87e7998a251710d2c31ae6cdb9cd2334f7ad4
George Lesica <george@lesica.com>
2023-08-16 15:54:09 -0600

    Add initial cli

---
+++ cmd/cli/flowork.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/alecthomas/kong"
+	"github.com/glesica/flowork/internal/app/cmd"
+	"log/slog"
+	"os"
+)
+
+var CLI struct {
+	cmd.GlobalOptions
+	Run *cmd.RunOptions `help:"Run a workflow" cmd:""`
+}
+
+func main() {
+	ctx := kong.Parse(
+		&CLI,
+		kong.Name("flowork"),
+		kong.Description("A simple runner for linear data workflows"),
+		kong.ShortUsageOnError(),
+	)
+
+	var level slog.Level
+	switch {
+	case CLI.Debug:
+		level = slog.LevelDebug
+	case CLI.Verbose:
+		level = slog.LevelInfo
+	}
+
+	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource: CLI.Debug,
+		Level:     level,
+	})
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+
+	err := ctx.Run(CLI.GlobalOptions)
+	if err != nil {
+		ctx.FatalIfErrorf(err)
+	}
+}
